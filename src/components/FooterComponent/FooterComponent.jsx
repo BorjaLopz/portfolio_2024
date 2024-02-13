846e0af9e5e6260b4ea0c138414c45d5ef90f639
Borja <lopzdiazborja@gmail.com>
2024-02-13 17:47:42 +0100

    Añadimos footer y animacion

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import "./styles.css"
+import React from "react";
+import "./styles.css";
+import redes from "../../redes.json";
+import { Link as ScrollLink } from "react-scroll";
 
 function FooterComponent() {
+  /* Obtenemos la fecha actual */
+  const fechaActual = new Date();
+  const añoActual = fechaActual.getFullYear();
+
+  const handleClick = () => {
+    alert("¡Gracias por visitarnos!");
+  };
+
   return (
-    <div>FooterComponent</div>
-  )
+    <section className="footerRedes">
+      <section className="redes">
+        {redes.map((r) => {
+          return (
+            <a
+              key={r?.id}
+              href={`${r?.name === "Email" ? `mailto:${r?.url}` : `${r?.url}`}`}
+              target="_blank"
+              className="cubeLink"
+            >
+              <article key={r?.id}>
+                <img src={`/Icons/${r?.icon}.svg`} alt={`${r?.icon} Icon`} />
+              </article>
+            </a>
+          );
+        })}
+      </section>
+      <p>
+        Borja López Díaz <span>© {añoActual}</span>
+      </p>
+      <ScrollLink
+        to="presentationComponentArticle"
+        smooth={true}
+        duration={300}
+        onClick={handleClick}
+        className="welcomeTop"
+      >
+        <img src="/Icons/ArrowUp.svg" alt="" />
+      </ScrollLink>
+    </section>
+  );
 }
 
-export default FooterComponent
+export default FooterComponent;
