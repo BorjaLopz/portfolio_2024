d7b66e9d0e855842b6b1415029baa014557c7115
Borja <lopzdiazborja@gmail.com>
2024-02-14 17:42:53 +0100

    Añadimos descarga del CV

--- src/components/DescriptionComponent/DescriptionComponent.jsx
+++ src/components/DescriptionComponent/DescriptionComponent.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./styles.css";
 
 function DescriptionComponent() {
+  const URL_CV = "/Borja López Díaz. CV. 2024.pdf";
   return (
     <section className="descriptionImage">
       <div className="hexagonImage"></div>
       <p className="description">
         Soy un apasionado desarrollador web fullstack. Mi formación sólida
         abarca el frontend y backend de sitios web.
+        <br />
+        Podras conocerme más a traves de{" "}
+        <span className="importantText">
+          <a href={URL_CV} download>
+            mi CV
+          </a>
+        </span>
       </p>
     </section>
   );
